f1658e69e4367500f1a34030718f6bc4c53b6885
Dave Joe M <davejoem@live.com>
2019-08-08 17:59:14 +0300

    Added settings page and updating options

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { Location } from '@angular/common'
+import { Deploy } from 'cordova-plugin-ionic/dist/ngx';
 
 @Component({
   selector: 'app-settings',
@@ -8,12 +9,56 @@ import { Location } from '@angular/common'
 })
 export class SettingsPage implements OnInit {
 
-  constructor(private location: Location) { }
+  constructor(
+    private deploy: Deploy,
+    private location: Location
+  ) { }
 
   ngOnInit() {
   }
 
   goBack() {
     this.location.back()
+  }
+
+  async performManualUpdate() {
+    const update = await this.deploy.checkForUpdate()
+    if (update.available) {
+      console.log('Update found');
+      alert('Update found')
+      await this.deploy.downloadUpdate((progress) => {
+        console.log('Downloading update...' + progress + '%');
+        alert('Downloading update')
+      })
+      await this.deploy.extractUpdate((progress) => {
+        console.log('Extracting update...' + progress + '%');
+        alert('Extracting update')
+      })
+      await this.deploy.reloadApp();
+      // this.splashScreen.hide();
+    } else {
+      console.log();
+      alert('No update found')
+    }
+  }
+
+
+  async performAutomaticUpdate() {
+    try {
+      const currentVersion = await this.deploy.getCurrentVersion();
+      const resp = await this.deploy.sync({ updateMethod: 'auto' }, percentDone => {
+        console.log(`Update is ${percentDone}% done!`);
+      });
+      if (currentVersion.versionId !== resp.versionId) {
+        // We found an update, and are in process of redirecting you since you put auto!
+      } else {
+        // No update available
+        console.log();
+        alert('No update found')
+      }
+    } catch (err) {
+      // We encountered an error.
+      this.performManualUpdate()
+    }
   }
 }
